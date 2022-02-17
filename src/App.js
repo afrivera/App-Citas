814fc64909b064f16cf44d3aeba6135ec6282fe4
arivera <andresrfelip@hotmail.com>
2022-02-16 21:39:35 -0500

    Configuración de componente formulario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import Formulario from "./components/Formulario";
 
 
 function App() {
+
+  // Arreglo de citas
+  const [citas, setCitas] = useState([]);
+
+  // funcion que toma las citas actuales y agrega una nueva
+  const crearCita = cita => {
+    setCitas([ ...citas, cita ]);
+  }
+
   return (
     <>
       <h1>Administrador de Pacientes</h1>
@@ -9,6 +19,7 @@ function App() {
         <div className="row">
           <div className="one-half column">
             <Formulario 
+              crearCita={ crearCita }
             />
           </div>
           <div className="one-half column">
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid} from 'uuid';
+// estado inicial formulario
+const initialState = {
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+};
 
-const Formulario = () => {
+const Formulario = ({ crearCita}) => {
+    // state de citas
+    const [cita, setCita] = useState(initialState);
+    const [error, setError] = useState(false);
+
+    // funcion que se ejecuta cuando el usuario escribe en los inputs
+    const handleChange = e => {
+        setCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    // extraer valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    // cuando se presiona agregar cita
+    const handleForm = e=> {
+        e.preventDefault();
+        
+        // validar campos
+        if(mascota.trim() === '' || propietario.trim() === '' || hora.trim() === '' || sintomas.trim() === '' || fecha.trim() === ''  ){
+            setError( true );
+            return;
+        }
+        // eliminar alerta
+        setError(false);
+
+        // asignar id
+        cita.id = uuid();
+
+        // crear cita
+        crearCita(cita);
+
+        // reiniciar form
+        setCita( initialState );
+
+    }
   return (
       <>
         <h2>Crear Cita</h2>
-        <form>
+        {
+            /* condicion si se presenta algún error de validacion en los campos */
+            error && <p className='alerta-error'>Todos los Campos son Obligatorios</p>
+        }
+        <form onSubmit={ handleForm }>
             <label>Nombre Mascota</label>
             <input
                 type="text"
                 name="mascota"
                 className='u-full-width'
                 placeholder='Nombre Mascota'
+                onChange={handleChange}
+                value= { mascota }
             />
             <label>Nombre Dueño</label>
             <input
@@ -18,23 +70,31 @@ const Formulario = () => {
                 name="propietario"
                 className='u-full-width'
                 placeholder='Nombre Dueño de la mascota'
+                onChange={handleChange}
+                value= { propietario }
             />
             <label>fecha</label>
             <input
                 type="date"
                 name="fecha"
                 className='u-full-width'
+                onChange={handleChange}
+                value= { fecha }
             />
             <label>Hora</label>
             <input
                 type="time"
                 name="hora"
                 className='u-full-width'
+                onChange={handleChange}
+                value= { hora }
             />
             <label>Sintomas</label>
             <textarea
                 className='u-full-width'
                 name='sintomas'
+                onChange={handleChange}
+                value= { sintomas }
             ></textarea>
             <button
                 type='submit'
